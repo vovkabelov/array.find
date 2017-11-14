1fa9e957974305f1a07a9dcaf1406ebb98a99934
Vladimir Belov <>
2017-11-14 15:09:33 +0200

    Tabs to spaces

--- src/array-find-polyfill.js
+++ src/array-find-polyfill.js
@@ -1,24 +1,24 @@
 ;(function() {
-	"use strict";
+  "use strict";
 
-	if (typeof Array.prototype.find !== "function") {
-		Array.prototype.find = function(callback, self) {
-			if (this === null) {
-				throw new TypeError('Cannot read property \'find\' of null');
-			}
+  if (typeof Array.prototype.find !== "function") {
+    Array.prototype.find = function(callback, self) {
+      if (this === null) {
+        throw new TypeError('Cannot read property \'find\' of null');
+      }
 
-			if (typeof callback !== "function")
-			{
-				throw new TypeError(typeof callback + ' is not a function');
-			}
+      if (typeof callback !== "function")
+      {
+        throw new TypeError(typeof callback + ' is not a function');
+      }
 
-			var arrLength = this.length;
+      var arrLength = this.length;
 
-			for (var i = 0; i < arrLength; i++) {
-				if (callback.call(self, this[i], i, this)) {
-					return this[i];
-				}
-			}
-		};
-	}
+      for (var i = 0; i < arrLength; i++) {
+        if (callback.call(self, this[i], i, this)) {
+          return this[i];
+        }
+      }
+    };
+  }
 })();
--- test/tests.js
+++ test/tests.js
@@ -5,84 +5,84 @@ require("../src/array-find-polyfill.js");
 
 
 describe("Array.prototype.find", function() {
-	var fruits = ["banana", "apple", "peach", "mango"];
-	var sparseArray = ["apple", , undefined, "peach"];
-	var arrayLikeObject = {0: "banana", 1: "apple", 2: "peach", 3: "mango", length: 4};
-	var arrayLikeNegativeObject = {0: "banana", 1: "apple", 2: "peach", 3: "mango", length: -4};
+  var fruits = ["banana", "apple", "peach", "mango"];
+  var sparseArray = ["apple", , undefined, "peach"];
+  var arrayLikeObject = {0: "banana", 1: "apple", 2: "peach", 3: "mango", length: 4};
+  var arrayLikeNegativeObject = {0: "banana", 1: "apple", 2: "peach", 3: "mango", length: -4};
 
-	it("Should be function", function() {
-		assert.equal(typeof Array.prototype.find, "function");
-	});
+  it("Should be function", function() {
+    assert.equal(typeof Array.prototype.find, "function");
+  });
 
-	it("Should find an element in an array", function() {
-		fruits.forEach(function(fruit, index) {
-			assert.equal(
-				fruits.find(function(fruit) {
-					return fruit === fruits[index];
-				}),
-				fruits[index]
-			);
-		});
-	});
+  it("Should find an element in an array", function() {
+    fruits.forEach(function(fruit, index) {
+      assert.equal(
+        fruits.find(function(fruit) {
+          return fruit === fruits[index];
+        }),
+        fruits[index]
+      );
+    });
+  });
 
-	it("Should be undefined if element not found", function() {
-		assert.equal(
-			fruits.find(function(fruit) {
-				return fruit === "plum";
-			}),
-			undefined
-		)
-	});
+  it("Should be undefined if element not found", function() {
+    assert.equal(
+      fruits.find(function(fruit) {
+        return fruit === "plum";
+      }),
+      undefined
+    )
+  });
 
-	it("Should work with a sparse array", function() {
-		var res = [];
-		sparseArray.find(function(item, index) {
-			res.push([index, item]);
-			return false;
-		});
+  it("Should work with a sparse array", function() {
+    var res = [];
+    sparseArray.find(function(item, index) {
+      res.push([index, item]);
+      return false;
+    });
 
-		assert.deepEqual(res, [[0, "apple"], [1, undefined], [2, undefined], [3, "peach"]]);
-	});
+    assert.deepEqual(res, [[0, "apple"], [1, undefined], [2, undefined], [3, "peach"]]);
+  });
 
-	it("Should work with an array-like object", function() {
-		assert.equal(
-			Array.prototype.find.call(arrayLikeObject, function(item) {
-				return item === "mango";
-			}),
-			"mango"
-		);
-	});
+  it("Should work with an array-like object", function() {
+    assert.equal(
+      Array.prototype.find.call(arrayLikeObject, function(item) {
+        return item === "mango";
+      }),
+      "mango"
+    );
+  });
 
-	it("Should not work with an array-like object with negative length", function() {
-		assert.equal(
-			Array.prototype.find.call(arrayLikeNegativeObject, function(item) {
-				return item === "mango";
-			}),
-			undefined
-		);
-	});
+  it("Should not work with an array-like object with negative length", function() {
+    assert.equal(
+      Array.prototype.find.call(arrayLikeNegativeObject, function(item) {
+        return item === "mango";
+      }),
+      undefined
+    );
+  });
 
-	it("Should be throws when call with null ass prototype", function() {
-		var tryFindOnNull = function() {
-			assert.equal(typeof Array.prototype.find, "function");
-			Array.prototype.find.call(null);
-		};
-		assert.throws(tryFindOnNull, TypeError);
-	});
+  it("Should be throws when call with null ass prototype", function() {
+    var tryFindOnNull = function() {
+      assert.equal(typeof Array.prototype.find, "function");
+      Array.prototype.find.call(null);
+    };
+    assert.throws(tryFindOnNull, TypeError);
+  });
 
-	it("Should be throws when call without callback function", function() {
-		var tryFindWithoutCallback = function() {
-			assert.equal(typeof Array.prototype.find, "function");
-			fruits.find();
-		};
-		assert.throws(tryFindWithoutCallback, TypeError);
-	});
+  it("Should be throws when call without callback function", function() {
+    var tryFindWithoutCallback = function() {
+      assert.equal(typeof Array.prototype.find, "function");
+      fruits.find();
+    };
+    assert.throws(tryFindWithoutCallback, TypeError);
+  });
 
-	it("Context must be equal to the specified object", function() {
-		var context = {};
+  it("Context must be equal to the specified object", function() {
+    var context = {};
 
-		fruits.find(function() {
-			assert.equal(context, this);
-		}, context);
-	});
+    fruits.find(function() {
+      assert.equal(context, this);
+    }, context);
+  });
 });
